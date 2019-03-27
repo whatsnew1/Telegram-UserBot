a720296e3c3da9f6d7c07e5a02a06e73ab76e16e
What's New <whatsnewseesomethingnew@gmail.com>
2019-03-27 08:45:37 +0530

    added Procfile

--- windows_startup_script.py
+++ windows_startup_script.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient, events
-API_KEY="Type that here"
-API_HASH="Type that here"
+API_KEY="733070"
+API_HASH="38dd9083ea624ff7076734610b3b9443"
 #get it from my.telegram.org
 bot = TelegramClient('userbot',API_KEY,API_HASH)
 bot.start()
